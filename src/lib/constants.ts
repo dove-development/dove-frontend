c5c660e77647fb41ab7c206d63bf36be91dd7ce5
eyrie64dev <eyrie64@proton.me>
2024-10-28 04:22:15 -0700

    Correct RPC address

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,7 @@ import { PublicKey } from "@solana/web3.js";
 export const DOVE_PROGRAM_ID = new PublicKey(
     "5WNHWKbpNuKxZT2uVTqQAorBVrLa42NAgPEFGtxSiM6N"
 );
-export const RPC_URL = "https://dove.money";
+export const RPC_URL = "https://rpc.dove.money";
 export const ASSET_DECIMALS = 4;
 export const DEBUG = false;
 export const MOCK_BACKEND = false;
