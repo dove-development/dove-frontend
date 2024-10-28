8123b14ff22e796deb5a330e2f704e7af32a87e7
eyrie64dev <eyrie64@proton.me>
2024-10-28 14:23:09 -0700

    Update constants for new deployment

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,9 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const DOVE_PROGRAM_ID = new PublicKey(
-    "5WNHWKbpNuKxZT2uVTqQAorBVrLa42NAgPEFGtxSiM6N"
+    "AnX6SwnicLbaUSCV4i1iNgLP4t1Y6ybudLBnrfyMpjmN"
 );
-export const RPC_URL = "http://127.0.0.1:22163";
+export const RPC_URL = "https://rpc.dove.money";
 export const ASSET_DECIMALS = 4;
 export const DEBUG = false;
 export const MOCK_BACKEND = false;
